3aaa1799f429c360f1b43a6c77290a6a3526b190
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2026-03-20 14:14:11 -0400

    Updating Colors and Formatting for Coming Soon Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,11 @@ import roundLogo from "../assets/Logo/roundlogo.png";
 function Home() {
   return (
     <div className="p-8 flex flex-col">
-      <h1 className="text-3xl font-bold text-brown-1">Coming Soon!</h1>
+      <h1 className="text-6xl font-bold text-brown-1 uppercase tracking-normal">
+        Coming Soon!
+      </h1>
       <img src={roundLogo} alt="Sukhibahn Logo" className=" mx-20" />
-      <p className="mt-4 text-brown-1">STUDIOS</p>
+      <p className="text-3xl mt-4 text-red-1">STUDIOS</p>
     </div>
   );
 }
