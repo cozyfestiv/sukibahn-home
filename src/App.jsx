c2e11e360c7f9c3e4a0a271407825574fada3c19
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2026-03-20 13:54:26 -0400

    Updating Tailwind, Setting up Coming Soon Page
    get tailwind to work,
    learn new tailwind standards 4.0
    add logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav className="bg-gray-800 text-white p-4">
+      {/* <nav className="bg-gray-800 text-white p-4">
         <ul className="flex space-x-4">
           <li>
             <Link to="/" className="hover:text-blue-300">Home</Link>
@@ -14,14 +14,14 @@ function App() {
             <Link to="/about" className="hover:text-blue-300">About</Link>
           </li>
         </ul>
-      </nav>
-      
+      </nav> */}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
+import roundLogo from "../assets/Logo/roundlogo.png";
+
 function Home() {
   return (
-    <div className="p-8">
-      <h1 className="text-3xl font-bold text-blue-600">Sukibahn Studios</h1>
-      <p className="mt-4">Coming Soon</p>
+    <div className="p-8 flex flex-col">
+      <h1 className="text-3xl font-bold text-brown-1">Coming Soon!</h1>
+      <img src={roundLogo} alt="Sukhibahn Logo" className=" mx-20" />
+      <p className="mt-4 text-brown-1">STUDIOS</p>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
