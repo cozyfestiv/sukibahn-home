3b9643e905263c3c9b5c1250624f7c720c7cf113
Gregory Topscher <126685397+cozyfestiv@users.noreply.github.com>
2026-06-26 10:25:53 -0400

    Add and Update Header
    Change header
    add icon
    format links

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import iconLogo from "../src/assets/Logo/Icon/horizlogo.png";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <nav className="bg-gray-800 text-white p-4">
-        <ul className="flex space-x-4">
-          <li>
-            <Link to="/" className="hover:text-blue-300">Home</Link>
-          </li>
-          <li>
-            <Link to="/about" className="hover:text-blue-300">About</Link>
-          </li>
-        </ul>
-      </nav> */}
+      <nav className="bg-bgtext-white p-4">
+        <div className="flex justify-between">
+          <img
+            src={iconLogo}
+            alt="Sukhibahn Icon Logo"
+            className=" md:mx-20 h-20"
+          />
+          <ul className="flex self-center space-x-4 font-bnfatporn text-red-2">
+            <li>
+              <Link to="/" className="hover:text-blue-300">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about" className="hover:text-blue-300">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
